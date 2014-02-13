bafc1f05e039d98ff08fb7ac9b15df3655258348
chischaschos <larin.s931@gmail.com>
2014-02-13 17:35:11 -0600

    With this commits api docs keeps the same features, but it adds the chance to customize the way you save to files the api docs

--- lib/api_docs.rb
+++ lib/api_docs.rb
@@ -7,10 +7,15 @@ module ApiDocs
 
   class << self
 
-    attr_accessor :store_data_strategy, :file_name_strategy
+    attr_accessor :store_data_strategy, :file_name_strategy, :docs,
+      :automatic_write
 
     def configure
       yield configuration
+    end
+
+    def automatic_write?
+      self.automatic_write.nil? ? true : self.automatic_write
     end
 
     def configuration
@@ -18,30 +23,31 @@ module ApiDocs
     end
     alias :config :configuration
 
-    def store_data(api_docs, captured_data)
+    def store_data(captured_data)
       strategy = store_data_strategy || method(:default_store_data)
-      strategy.call(api_docs, captured_data)
+      strategy.call(docs, captured_data, default_key(captured_data))
     end
 
-    def file_name(captured_data)
-      strategy = file_name_strategy || method(:default_file_name)
-      strategy.call(captured_data)
+    def docs
+      @docs ||= Hash.new
     end
 
     private
 
-    def default_store_data(api_docs, captured_data)
-      # Marking response as an unique
+    def default_store_data(api_docs, captured_data, key)
+      api_docs[captured_data['action']] ||= { }
+      api_docs[captured_data['action']][key] = captured_data
+    end
+
+    # Marking response as an unique
+    def default_key(captured_data)
       key = ""
       key << captured_data['method']
       key << captured_data['path']
       key << captured_data['meta'].to_s
       key << captured_data['params'].to_s
       key << captured_data['status'].to_s
-      hashed_key = 'ID-' + Digest::MD5.hexdigest(key)
-
-      api_docs[captured_data['action']] ||= { }
-      api_docs[captured_data['action']][hashed_key] = captured_data
+      'ID-' + Digest::MD5.hexdigest(key)
     end
 
     def default_file_name(captured_data)
--- lib/api_docs/test_helper.rb
+++ lib/api_docs/test_helper.rb
@@ -11,14 +11,12 @@ module ApiDocs::TestHelper
       parsed_path   = path.dup
       parsed_params = params.dup
 
-
       parsed_params.each do |k, v|
         parsed_params.delete(k) if parsed_path.gsub!(":#{k}", v.to_s)
       end
 
       # Making actual test request. Based on the example above:
       #   get '/users/12345'
-
       send(method, parsed_path, parsed_params, headers)
 
       meta = Hash.new
@@ -29,7 +27,7 @@ module ApiDocs::TestHelper
         return unless ENV['API_DOCS']
       end
 
-      @captured_data = {
+      captured_data = {
         'controller'  => request.filtered_parameters['controller'],
         'action'      => request.filtered_parameters['action'],
         'meta'        => meta,
@@ -41,31 +39,34 @@ module ApiDocs::TestHelper
         'body'        => response.body
       }
 
-      ApiDocs.store_data(api_docs, @captured_data)
-
-      write_docs
+      read_api_docs(captured_data) if ApiDocs.automatic_write?
+      ApiDocs.store_data(captured_data)
+      write_api_docs(captured_data) if ApiDocs.automatic_write?
     end
 
-    def api_docs
-      @api_docs ||= if File.exists?(file_path)
-        YAML.load_file(file_path) rescue Hash.new
+    def read_api_docs(captured_data)
+      calculated_file_path = file_path(captured_data)
+      ApiDocs.docs = if File.exists?(calculated_file_path)
+        YAML.load_file(calculated_file_path) rescue Hash.new
       else
         Hash.new
       end
     end
 
-    def file_name
-      ApiDocs.file_name(@captured_data)
+    def file_path(captured_data)
+      File.expand_path(file_name(captured_data), ApiDocs.config.docs_path)
     end
 
-    def file_path
-      File.expand_path(file_name, ApiDocs.config.docs_path)
+    def file_name(captured_data)
+      "#{captured_data['controller'].gsub('/', ':')}.yml"
     end
 
-    def write_docs
-      FileUtils.mkdir_p(File.dirname(file_path))
-      File.open(file_path, 'w'){|f| f.write(api_docs.to_yaml)}
+    def write_api_docs(captured_data)
+      calculated_file_path = file_path(captured_data)
+      FileUtils.mkdir_p(File.dirname(calculated_file_path))
+      File.open(calculated_file_path, 'w') {|f| f.write(ApiDocs.docs.to_yaml)}
     end
+
   end
 
   # Cleans up params. Removes things like File object handlers
@@ -74,10 +75,10 @@ module ApiDocs::TestHelper
     case params
     when Hash
       params.each_with_object({}) do |(key, value), res|
-        res[key.to_s] = ApiDocs::TestHelper.api_deep_clean_params(value)
+        res[key.to_s] = api_deep_clean_params(value)
       end
     when Array
-      params.collect{|value| ApiDocs::TestHelper.api_deep_clean_params(value)}
+      params.collect{|value| api_deep_clean_params(value)}
     else
       case params
       when Rack::Test::UploadedFile
